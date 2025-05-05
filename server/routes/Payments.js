5ff351399cc5749b4d9f9c0e8a5a4a1fa83d55a8
anshuman008 <code.anshu@gmail.com>
2025-05-05 14:08:37 +0530

    feat: stripe payment route added

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -1,11 +1,19 @@
 // Import the required modules
 const express = require("express")
 const router = express.Router()
+const  { stripePayement } = require("../controllers/StripePayement");
+
 
 const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
-const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
+const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth");
+const { BsRewind } = require("react-icons/bs");
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment",auth, isStudent, verifyPayment)
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
+
+
+router.post("/test",stripePayement);
+
+
 
 module.exports = router
